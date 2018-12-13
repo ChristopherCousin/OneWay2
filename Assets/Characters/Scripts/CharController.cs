03da454a6b12ab0fe2852f065613cfb65387304c
Christopher C.A <tomasadison2014@gmail.com>
2018-12-13 16:41:11 +0100

    Character functions
    Added movement and animations

--- Assets/Characters/Scripts/CharController.cs
+++ Assets/Characters/Scripts/CharController.cs
@@ -1,16 +1,22 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEditor.Animations;
 
 public class CharController : MonoBehaviour {
     [SerializeField]
     public int _MovementSpeed = 5;
     private Vector2 direction;
     private Animator _animator;
+    int animationCount;
+    List<string> animationsList = new List<string>();
 
 	void Start () {
         _animator = gameObject.GetComponent<Animator>();
-	}
+        animationCount = _animator.parameterCount;
+        getAnimationsParameters();
+
+    }
 	
 	// Update is called once per frame
 	void Update () {
@@ -26,17 +32,65 @@ public class CharController : MonoBehaviour {
     private void GetInput()
     {
         direction = Vector2.zero;
-        if (Input.GetKey(KeyCode.W)) direction = Vector2.up;
+
+        if (Input.GetKey(KeyCode.W))
+        {
+            direction = Vector2.up;
+            setAnimation("walkUp", true);
+        }
+        else if (Input.GetKeyUp(KeyCode.W))
+        {
+            setAnimation("walkUp", false);
+        }
         if (Input.GetKey(KeyCode.A))
         {
             direction = Vector2.left;
-            _animator.SetBool("walkLeft", true);
-        } else
-        {
-            _animator.SetBool("walkLeft", false);
+            setAnimation("walkLeft", true);
         }
-        if (Input.GetKey(KeyCode.S)) direction = Vector2.down;
-        if (Input.GetKey(KeyCode.D)) direction = Vector2.right;
+        else if (Input.GetKeyUp(KeyCode.A))
+        {
+            setAnimation("walkLeft", false);
+        }
+        if (Input.GetKey(KeyCode.S))
+        {
+            direction = Vector2.down;
+            setAnimation("walkDown", true);
+        }
+        else if (Input.GetKeyUp(KeyCode.S))
+        {
+            setAnimation("walkDown", false);
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+            direction = Vector2.right;
+            setAnimation("walkRigth", true);
+        }
+        else if (Input.GetKeyUp(KeyCode.D))
+        {
+            setAnimation("walkRigth", false);
+        }
     }
 
+    private void setAnimation(string value, bool booleana)
+    {
+        foreach(string animationName in animationsList)
+        {
+            if (value.Equals(animationName) && booleana)
+            {
+                _animator.SetBool(animationName, true);
+            }
+            if (value.Equals(animationName) && !booleana)
+            {
+                _animator.SetBool(animationName, false);
+            }
+        }
+    }
+
+    private void getAnimationsParameters()
+    {
+        for(int x = 0; x < animationCount; x++)
+        {
+            animationsList.Add(_animator.GetParameter(x).name);
+        }
+    }
 }
